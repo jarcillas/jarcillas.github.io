58ba8cc1a18fed3a4031ceff828f13b1baafd3a4
Jesreal Arcillas <jesrealarcillas@gmail.com>
2024-07-30 22:06:18 +0800

    Add dadjoke button

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,43 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+  const [dadJoke, setDadJoke] = useState();
+
+  const fetchDadJoke = async () => {
+    try {
+      // Fetch a dad joke
+      const response = await fetch("https://icanhazdadjoke.com/", {
+        headers: {
+          Accept: "application/json",
+        },
+      });
+      const result = await response.json();
+      setDadJoke(result.joke);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setDadJoke("Error fetching joke.");
+    }
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1 className="header">
+        <a href="/">jarcillas.github.io</a>
+      </h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <button onClick={() => setCount(0)}>reset</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="box">
+        <button onClick={fetchDadJoke}>Fetch a Dad Joke</button>
+        <p className="dadjoke">{dadJoke || ""}</p>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
