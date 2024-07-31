ef19e457738d248e100acaca07ca88137fe8f53e
Jesreal Arcillas <jesrealarcillas@gmail.com>
2024-07-31 17:14:17 +0800

    Add useFetch hook

---
+++ src/hooks/useFetch.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+export const useFetch = (url, options) => {
+  const [data, setData] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      // Fetch a dad joke
+      const response = await fetch(url, options);
+      const result = await response.json();
+      setData(result);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setError(error);
+    }
+
+    setIsLoading(false);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return { data, isLoading, error };
+};
