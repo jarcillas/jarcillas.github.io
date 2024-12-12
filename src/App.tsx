b35e111bc0b8f0667ef03ab5dfbd5ac2736bdf2c
Jesreal Arcillas <jesrealarcillas@gmail.com>
2024-12-12 12:48:32 +0800

    add hover animation to site title

--- src/App.tsx
+++ src/App.tsx
@@ -16,16 +16,19 @@ function App() {
 
   return (
     <>
-      <h1 className="w-full flex justify-center p-2 font-title text-4xl font-extralight">
-        <a href="/">jarcillas.github.io</a>
+      <h1 className="w-full flex justify-center px-4 pt-2 pb-3 font-title text-2xl align-middle font-normal border-b-slate-400 border-2">
+        <a className="hover:animate-tada" href="/">
+          jarcillas.github.io
+        </a>
       </h1>
 
-      <div className="w-full">
-        <p className="font-script text-2xl text-slate-800">
-          {showDadJoke ? data?.joke : ""}
-        </p>
-        <div className="buttons">
+      <div className="w-full flex flex-col justify-center items-center gap-y-4">
+        <div className="font-title text-3xl text-slate-900 font-extralight p-8 w-fit rounded-lg tracking-tight text-center">
+          {showDadJoke ? data?.joke : "No dad joke yet."}
+        </div>
+        <div className="flex items-center gap-4">
           <button
+            className="p-2 bg-slate-900 text-slate-300 rounded-lg hover:bg-slate-700 text-lg font-title"
             onClick={() => {
               setShowDadJoke(true);
               fetchData();
@@ -35,6 +38,7 @@ function App() {
             {isLoading ? "Loading..." : "Fetch a Dad Joke "}
           </button>
           <button
+            className="p-2 bg-slate-900 text-slate-300 rounded-lg hover:bg-slate-700 text-lg font-title"
             onClick={() => {
               setShowDadJoke(false);
             }}
