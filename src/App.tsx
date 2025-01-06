1cb4ee33046fc03d7695859b73ff396d4cfb45d0
Jesreal Arcillas <jesrealarcillas@gmail.com>
2025-01-06 10:04:43 +0800

    fix height of dadjoke component

--- src/App.tsx
+++ src/App.tsx
@@ -22,29 +22,31 @@ function App() {
         </a>
       </h1>
 
-      <div className="w-full flex flex-col justify-center items-center gap-y-4">
-        <div className="font-title text-3xl text-slate-900 font-extralight p-8 w-fit rounded-lg tracking-tight text-center">
-          {showDadJoke ? data?.joke : "No dad joke yet."}
-        </div>
-        <div className="flex items-center gap-4">
-          <button
-            className="p-2 bg-slate-900 text-slate-300 rounded-lg hover:bg-slate-700 text-lg font-title"
-            onClick={() => {
-              setShowDadJoke(true);
-              fetchData();
-            }}
-            disabled={isLoading}
-          >
-            {isLoading ? "Loading..." : "Fetch a Dad Joke "}
-          </button>
-          <button
-            className="p-2 bg-slate-900 text-slate-300 rounded-lg hover:bg-slate-700 text-lg font-title"
-            onClick={() => {
-              setShowDadJoke(false);
-            }}
-          >
-            Clear
-          </button>
+      <div className="w-full flex flex-col items-center">
+        <div className="w-full flex flex-col justify-center items-center gap-y-4 container">
+          <div className="font-title text-3xl text-slate-900 font-extralight flex items-center h-[120px] p-2 w-fit rounded-lg tracking-tight text-center text-balance">
+            {showDadJoke ? data?.joke : "No dad joke yet."}
+          </div>
+          <div className="flex items-center gap-4">
+            <button
+              className="py-2 px-4 w-[200px] bg-slate-900 text-slate-300 rounded-lg hover:bg-slate-700 text-lg font-title"
+              onClick={() => {
+                setShowDadJoke(true);
+                fetchData();
+              }}
+              disabled={isLoading}
+            >
+              {isLoading ? "Loading..." : "Fetch a Dad Joke "}
+            </button>
+            <button
+              className="py-2 px-4 w-[200px] bg-slate-900 text-slate-300 rounded-lg hover:bg-slate-700 text-lg font-title"
+              onClick={() => {
+                setShowDadJoke(false);
+              }}
+            >
+              Clear
+            </button>
+          </div>
         </div>
       </div>
     </>
