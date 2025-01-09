eee636c0b809baea4ea772699f86c6c00350724e
Jesreal Arcillas <jesrealarcillas@gmail.com>
2025-01-09 14:01:24 +0800

    set the navbar to be fixed at the top

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,30 @@ import "./App.css";
 
 function App() {
   return (
-    <>
-      <h1 className="w-full flex justify-center px-4 pt-2 pb-3 font-title text-2xl align-middle font-normal border-b-slate-400 border-2">
-        <a className="hover:animate-tada" href="/">
-          jarcillas.github.io
-        </a>
+    <div className="overflow-hidden">
+      <h1 className="w-full flex fixed justify-between items-center mt-0 px-4 pt-2 pb-3 font-title text-2xl font-normal border-b-slate-400 border-[1px] bg-white">
+        <div>
+          <a className="hover:animate-tada" href="/">
+            jarcillas.github.io
+          </a>
+        </div>
+        <div className="flex items-center space-x-4 text-xl ">
+          <a href="/" className="hover:animate-tada">
+            Projects
+          </a>
+          <a href="/" className="hover:animate-tada">
+            Contact
+          </a>
+          <a href="/" className="hover:animate-tada">
+            About
+          </a>
+        </div>
       </h1>
 
-      <div className="w-full flex flex-col items-center">
+      <div className="w-full mt-14 flex flex-col items-center">
         <DadJoke />
       </div>
-    </>
+    </div>
   );
 }
 
