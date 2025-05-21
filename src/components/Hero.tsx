4b3ef938143af695667b48c3b2c178c9ff9a96b3
Jesreal Arcillas <jesrealarcillas@gmail.com>
2025-05-21 10:29:24 +0800

    add profile photo

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,14 +1,20 @@
+import profilePhoto from "../assets/profile-photo.jpg";
+
 const Hero = () => {
   return (
-    <div className="min-h-screen bg-base-200 flex flex-row px-16 w-full justify-between">
-      <div className="text-center text-primary font-title">
+    <div className="flex-grow max-h-160 bg-base-200 flex flex-row w-full justify-between items-stretch">
+      <div className="text-center text-primary font-title flex-1/3 flex flex-col justify-center">
         <div className="max-w-md">
-          <h1 className="text-5xl font-bold mb-4">Jesreal Arcillas</h1>
-          <h2 className="text-3xl">Front End Developer</h2>
+          <img className="rounded-full size-64" src={profilePhoto}></img>
         </div>
       </div>
-      <div className="text-center text-primary font-title">
-        <div className="max-w-md"></div>
+      <div className="text-primary font-title flex items-center flex-2/3">
+        <div className="max-w-md font-mono select-none">
+          <h1 className="text-[120px] font-bold mb-4 leading-[120px] glow">
+            JESREAL ARCILLAS
+          </h1>
+          <h2 className="text-[38px]">Front End Developer</h2>
+        </div>
       </div>
     </div>
   );
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,8 +1,9 @@
 const Navbar = () => (
-  <h1 className="w-full flex fixed justify-between items-center mt-0 px-8 pt-2 pb-3 font-title text-2xl font-normal text-primary">
+  <h1 className="w-full flex flex-col gap-4 md:gap-2 md:flex-row fixed justify-between items-center mt-0 px-8 pt-2 pb-3 font-mono text-xl font-normal text-primary">
     <div>
       <a className="hover:animate-tada" href="/">
-        jarcillas
+        <span className="glow">jarcillas</span>
+        <span className="hidden md:inline">.github.io</span>
       </a>
     </div>
     <div className="flex items-center space-x-16 text-xl">
