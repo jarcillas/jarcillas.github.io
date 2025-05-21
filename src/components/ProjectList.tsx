2d5476f743c2a9dd0e5a9954d67d9941b9f700c8
Jesreal Arcillas <jesrealarcillas@gmail.com>
2025-05-21 14:08:48 +0800

    add heading to projects list

--- src/components/ProjectList.tsx
+++ src/components/ProjectList.tsx
@@ -3,20 +3,23 @@ import { ProjectItem } from "./ProjectItem";
 
 const ProjectList = ({ list }: { list: ProjectItemType[] }) => {
   return (
-    <div
-      className="flex flex-row gap-6 items-stretch w-full max-w-5xl mx-auto mt-8 overflow-x-auto pb-4 project-list-scrollbar font-mono"
-      style={{
-        WebkitOverflowScrolling: "touch",
-      }}
-      onWheel={(e) => {
-        if (e.deltaY !== 0) {
-          e.currentTarget.scrollLeft += e.deltaY;
-        }
-      }}
-    >
-      {list.map((item, idx) => (
-        <ProjectItem key={item.title + idx} item={item} />
-      ))}
+    <div className="flex flex-col items-center w-full max-w-5xl mx-auto mt-8">
+      <h2 className="text-xl text-primary font-mono w-full">projects</h2>
+      <div
+        className="flex flex-row gap-6 items-stretch w-full max-w-5xl mx-auto mt-8 overflow-x-auto pb-4 project-list-scrollbar font-mono"
+        style={{
+          WebkitOverflowScrolling: "touch",
+        }}
+        onWheel={(e) => {
+          if (e.deltaY !== 0) {
+            e.currentTarget.scrollLeft += e.deltaY / 2;
+          }
+        }}
+      >
+        {list.map((item, idx) => (
+          <ProjectItem key={item.title + idx} item={item} />
+        ))}
+      </div>
     </div>
   );
 };
